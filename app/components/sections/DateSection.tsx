09269c172f9b0bc427066aa6271c199d0d296e1e
Manuel Guzman <manuguzman8@gmail.com>
2026-02-24 11:49:37 -0600

    feat: new date section and present section

---
+++ app/components/sections/DateSection.tsx
@@ -0,0 +1,333 @@
+// app/components/sections/DateSection.tsx
+const DateSection = () => (
+    <>
+        <path
+            d="M51.975 716.295q-1.444 0-3.016.552a6.2 6.2 0 0 0-2.676 1.7q-1.062 1.147-1.062 3.1 0 1.615.807 2.89a9.7 9.7 0 0 0 2.03 2.133q-1.05.529-1.945 1.223-1.657 1.275-2.592 2.888a6.9 6.9 0 0 0-.892 3.399q0 1.912 1.062 3.526 1.062 1.572 3.017 2.549 1.953.977 4.588.977 2.55 0 4.673-.935 2.166-.976 3.44-2.676 1.318-1.7 1.318-3.908 0-1.911-.935-3.272a8.6 8.6 0 0 0-2.336-2.42 36 36 0 0 0-2.932-1.998l-.258-.165q1.89-.864 2.935-2.256 1.147-1.57 1.147-2.889 0-1.02-.765-2.04-.723-1.019-2.167-1.699-1.4-.678-3.44-.679zm-.424.892q1.275 0 2.21 1.19.977 1.147.977 3.058a7.5 7.5 0 0 1-.34 2.21q-.262.938-.658 1.875-.979-.63-1.891-1.238-1.275-.892-2.082-1.954a3.84 3.84 0 0 1-.807-2.38q0-1.19.68-1.954.72-.807 1.911-.807m-22.812 7.095q-.085 0-.085.254 0 .255.085.255 1.443 0 2.124.213.722.213.934.85.213.636.213 1.869v9.516q0 1.23-.255 1.87-.255.637-.977.849-.68.212-2.04.212-.085 0-.085.255t.085.255q1.105 0 2.422-.043a92 92 0 0 1 5.693 0q1.36.043 2.42.043.13 0 .13-.255t-.13-.255q-1.443 0-2.165-.212-.679-.213-.893-.85t-.212-1.87v-9.515q0-1.231.255-1.87.255-.636.934-.85.723-.211 2.082-.212.128 0 .128-.254 0-.255-.128-.255-1.062 0-2.421.042-1.36.042-2.847.042a90 90 0 0 1-2.846-.042 76 76 0 0 0-2.42-.042m20.253 3.087q.89.643 1.836 1.246 1.487.892 2.847 1.827t2.251 2.124q.892 1.146.892 2.846 0 1.23-.51 2.38a4.7 4.7 0 0 1-1.486 1.826q-.978.68-2.38.68-1.571 0-2.803-1.02-1.233-1.02-1.954-2.677-.68-1.656-.68-3.568 0-2.21.892-4.036a7.8 7.8 0 0 1 1.094-1.627zm-13.783 22.77.001.001q-2.636 0-4.716 1.104a8.4 8.4 0 0 0-3.23 3.06q-1.146 1.952-1.146 4.502t1.147 4.545a8.6 8.6 0 0 0 3.23 3.144q2.081 1.147 4.714 1.147t4.63-1.104q2.04-1.148 3.187-3.101 1.147-1.998 1.147-4.546 0-2.55-1.147-4.503a7.7 7.7 0 0 0-3.144-3.101q-1.997-1.148-4.673-1.148m24.607.032q-.159 0-.223.096l-13.17 15.973q-.17.17-.043.34.17.17.34.17 2.89 0 5.183.042 2.337 0 4.248.043v7.987q0 .467.383.254l3.356-1.656q.34-.17.34-.595v-5.89q1.433.04 2.804.112.128 0 .17-.51.085-.51.042-.976 0-.51-.127-.51-1.51.038-2.889.11v-14.724q0-.213-.297-.255a.5.5 0 0 0-.117-.01m-24.948.86q1.232 0 2.294.85 1.105.849 1.742 2.718.68 1.827.68 4.886 0 3.356-.977 5.31-.978 1.954-2.974 1.954-1.488 0-2.634-1.062-1.104-1.062-1.742-2.931-.594-1.911-.595-4.418 0-3.315 1.062-5.31 1.105-1.998 3.144-1.997m21.284 5.642v8.592c-.867.01-1.786.018-2.761.04h-4.12a.6.6 0 0 1-.256-.042q-.086-.086.043-.255zm3.664 24.725c-.01 0-.021 0-.031.01q-3.673 1.236-6.338 3.543-2.633 2.307-4.062 5.232-1.398 2.925-1.398 5.98 0 3.381 1.69 5.265 1.723 1.885 4.583 1.885 1.885 0 3.412-.747 1.56-.747 2.47-2.015.943-1.301.943-2.827 0-1.008-.455-1.95a3.6 3.6 0 0 0-1.365-1.593q-.91-.65-2.438-.65-1.43 0-2.502.585a7 7 0 0 0-1.918 1.462q-.03.098.098.228.162.097.26 0 .65-.454 1.17-.683a2.8 2.8 0 0 1 1.137-.227q1.398 0 2.048 1.072.65 1.04.65 2.698 0 1.852-.715 2.86t-1.918 1.007q-1.17 0-2.047-.91-.845-.942-1.333-2.502-.455-1.56-.455-3.413 0-2.632.943-5.297.975-2.666 2.892-4.94 1.918-2.276 4.81-3.705.097-.065.032-.228-.058-.143-.163-.135zM30.82 784.68q-.113-.001-.14.08l-1.94 7.533q-.045.09.22.132.265.045.309-.044.925-2.643 1.938-4.23 1.059-1.63 2.82-1.63 1.63 0 2.423 1.278.795 1.234.793 2.776a5.1 5.1 0 0 1-.396 1.939q-.397.925-1.41 2.115-.969 1.19-2.688 2.863a203 203 0 0 1-4.317 4.054q-.221.175-.088.705.133.485.352.485h13.482a.3.3 0 0 0 .265-.133.6.6 0 0 0 .132-.352l.925-5.243q0-.089-.264-.133-.22-.045-.265.133a2.1 2.1 0 0 1-.88 1.233q-.66.442-1.807.661t-2.864.265a153 153 0 0 1-3.92.044q-.178 0-.266-.088-.088-.09.044-.22a98 98 0 0 1 2.555-1.895 33 33 0 0 0 2.776-2.16q1.366-1.188 2.247-2.555.882-1.365.88-2.951 0-1.851-1.365-2.997-1.32-1.19-3.789-1.19-1.85 0-2.952.486-1.1.483-1.85.484a.88.88 0 0 1-.573-.22q-.22-.22-.044-1.058.089-.088-.176-.132a.6.6 0 0 0-.167-.035"
+            style={{
+                fontSize: "113.841px",
+                lineHeight: 0.9,
+                fontFamily: "&quot",
+                fontVariationSettings: "&quot",
+                textAlign: "center",
+                letterSpacing: 0,
+                wordSpacing: "-.486833px",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.562494,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        />
+        <text
+            xmlSpace="preserve"
+            x={289.044}
+            y={737.904}
+            style={{
+                fontStyle: "italic",
+                fontVariant: "normal",
+                fontWeight: 400,
+                fontStretch: "normal",
+                fontSize: "14.1892px",
+                lineHeight: 1,
+                fontFamily: "&quot",
+                fontVariantLigatures: "normal",
+                fontVariantCaps: "normal",
+                fontVariantNumeric: "normal",
+                fontVariantEastAsian: "normal",
+                textAlign: "center",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.202815,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        >
+            <tspan
+                x={289.044}
+                y={737.904}
+                style={{
+                    fontStyle: "italic",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontSize: "14.1892px",
+                    fontFamily: "&quot",
+                    fontVariantLigatures: "normal",
+                    fontVariantCaps: "normal",
+                    fontVariantNumeric: "normal",
+                    fontVariantEastAsian: "normal",
+                    textAlign: "end",
+                    textAnchor: "end",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.202815,
+                }}
+            >
+                {"Con el coraz\xF3n lleno de amor y gratitud,"}
+            </tspan>
+            <tspan
+                x={289.044}
+                y={752.093}
+                style={{
+                    fontStyle: "italic",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontSize: "14.1892px",
+                    fontFamily: "&quot",
+                    fontVariantLigatures: "normal",
+                    fontVariantCaps: "normal",
+                    fontVariantNumeric: "normal",
+                    fontVariantEastAsian: "normal",
+                    textAlign: "end",
+                    textAnchor: "end",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.202815,
+                }}
+            >
+                {"los invitamos a celebrar con nosotros"}
+            </tspan>
+            <tspan
+                x={289.044}
+                y={766.282}
+                style={{
+                    fontStyle: "italic",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontSize: "14.1892px",
+                    fontFamily: "&quot",
+                    fontVariantLigatures: "normal",
+                    fontVariantCaps: "normal",
+                    fontVariantNumeric: "normal",
+                    fontVariantEastAsian: "normal",
+                    textAlign: "end",
+                    textAnchor: "end",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.202815,
+                }}
+            >
+                {"el d\xEDa m\xE1s importante de nuestras vidas."}
+            </tspan>
+            <tspan
+                x={289.044}
+                y={780.471}
+                style={{
+                    fontStyle: "italic",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontSize: "14.1892px",
+                    fontFamily: "&quot",
+                    fontVariantLigatures: "normal",
+                    fontVariantCaps: "normal",
+                    fontVariantNumeric: "normal",
+                    fontVariantEastAsian: "normal",
+                    textAlign: "end",
+                    textAnchor: "end",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.202815,
+                }}
+            >
+                {"Su presencia ser\xE1 un regalo"}
+            </tspan>
+            <tspan
+                x={289.044}
+                y={794.661}
+                style={{
+                    fontStyle: "italic",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontSize: "14.1892px",
+                    fontFamily: "&quot",
+                    fontVariantLigatures: "normal",
+                    fontVariantCaps: "normal",
+                    fontVariantNumeric: "normal",
+                    fontVariantEastAsian: "normal",
+                    textAlign: "end",
+                    textAnchor: "end",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.202815,
+                }}
+            >
+                {"inolvidable en nuestra boda."}
+            </tspan>
+        </text>
+        <text
+            xmlSpace="preserve"
+            x={156.929}
+            y={1075.719}
+            style={{
+                fontSize: "71.182px",
+                fontFamily: "&quot",
+                fontVariationSettings: "&quot",
+                textAlign: "center",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.942497,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        >
+            <tspan
+                x={156.929}
+                y={1075.719}
+                style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontFamily: "&quot",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.942497,
+                }}
+            >
+                {"18"}
+            </tspan>
+        </text>
+        <text
+            xmlSpace="preserve"
+            x={80.424}
+            y={1066.498}
+            style={{
+                fontSize: "14.6075px",
+                fontFamily: "&quot",
+                fontVariationSettings: "&quot",
+                textAlign: "center",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.193413,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        >
+            <tspan
+                x={80.424}
+                y={1066.498}
+                style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontFamily: "&quot",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.193413,
+                }}
+            >
+                {"S\xC1BADO"}
+            </tspan>
+        </text>
+        <text
+            xmlSpace="preserve"
+            x={234.446}
+            y={1066.498}
+            style={{
+                fontSize: "14.6075px",
+                fontFamily: "&quot",
+                fontVariationSettings: "&quot",
+                textAlign: "center",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.193413,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        >
+            <tspan
+                x={234.446}
+                y={1066.498}
+                style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontFamily: "&quot",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.193413,
+                }}
+            >
+                {"ABRIL"}
+            </tspan>
+        </text>
+        <text
+            xmlSpace="preserve"
+            x={157.264}
+            y={996.265}
+            style={{
+                fontSize: "15.9109px",
+                fontFamily: "&quot",
+                fontVariationSettings: "&quot",
+                textAlign: "center",
+                textAnchor: "middle",
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                strokeWidth: 0.210671,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        >
+            <tspan
+                x={157.264}
+                y={996.265}
+                style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontFamily: "&quot",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.210671,
+                }}
+            >
+                {"Te esperamos para"}
+            </tspan>
+            <tspan
+                x={157.264}
+                y={1016.36}
+                style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: 400,
+                    fontStretch: "normal",
+                    fontFamily: "&quot",
+                    fill: "#e5e5e5",
+                    fillOpacity: 1,
+                    strokeWidth: 0.210671,
+                }}
+            >
+                {"celebrar nuestra boda el"}
+            </tspan>
+        </text>
+        <path
+            d="M38.017 1044.92h85.178v.928H38.017zm0 31.702h85.178v.928H38.017zm153.643-31.702h85.178v.928H191.66zm0 31.702h85.178v.928H191.66z"
+            style={{
+                fontVariationSettings: "&quot",
+                opacity: 0.922662,
+                fill: "#e5e5e5",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 1.30641,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+            }}
+        />
+
+
+    </>
+);
+
+export default DateSection;
