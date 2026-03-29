51e9a76502aa32292d5db9a9fff99be6c58b9771
Manuel Guzman <manuguzman8@gmail.com>
2026-03-28 23:32:49 -0600

    feat: restructure project and remove unused files

--- jest.config.js
+++ jest.config.js
@@ -9,13 +9,13 @@ const config = {
     ...tsJestTransformCfg,
   },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
     "^@/invitation/(.*)$": "<rootDir>/src/invitation/$1",
     "^@/guests/(.*)$": "<rootDir>/src/guests/$1",
     "^@/ceremony/(.*)$": "<rootDir>/src/ceremony/$1",
     "^@/venue/(.*)$": "<rootDir>/src/venue/$1",
     "^@/rsvp/(.*)$": "<rootDir>/src/rsvp/$1",
     "^@/shared/(.*)$": "<rootDir>/src/_shared/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
